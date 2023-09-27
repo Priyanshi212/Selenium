2806117d500cb264139ce3b3f25ba8103f2dc20f
Priyanshi212 <50592971+Priyanshi212@users.noreply.github.com>
2023-09-27 16:02:10 +0530

    Add files via upload
    First commit

---
+++ DemoAutomation.java
@@ -0,0 +1,27 @@
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.FluentWait;
+
+public class DemoAutomation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Browser_driver\\chromedriver.exe");
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+		//driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10000));
+		driver.findElement(By.name("q")).sendKeys("selenium");
+		driver.findElement(By.name("q")).sendKeys(Keys.RETURN);
+		
+		driver.findElement(By.xpath("//*[@id=\"rso\"]/div[1]/div/div/div/div/div/div/div/div[1]/div/span/a/h3")).click();
+		driver.close();
+		
+	}
+
+}
